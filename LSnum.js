46cdcafb2f4f8403994ad8fc3509e36d6b74b87b
Mefistic <92448706+Mefistic@users.noreply.github.com>
2022-12-08 17:40:54 -0300

    Fixed Many Bugs.

--- LSnum.js
+++ LSnum.js
@@ -3,193 +3,168 @@
 
 class LSnum
 {
-	constructor(num)
+	constructor(num = 0)
 	{
-		if (num.l) this.l = num.l
+		if (num instanceof LSnum) this.l = num.l
+
 		if (typeof(num) == 'string')
 		{
 			var indexE = num.indexOf('e')
 			
-			if (indexE == -1)
-			{
-				this.l = Math.log10(num)
-			}
+			if (indexE == -1) this.l = Math.log10(num)
 			
-			else if (indexE == 0)
-			{
-				this.l = Number(num.substring(1))
-			}
+			else if (indexE == 0) this.l = Number(num.substring(1))
 			
-			else
-			{
-				this.l = Math.log10(num.substring(0, indexE)) + Number(num.substring(indexE+1))
-			}
+			else this.l = Math.log10(num.substring(0, indexE)) + Number(num.substring(indexE+1))
 		}
 		else if (typeof(num) == 'number') this.l = Math.log10(num)
 	}
-	
+
 	add(x)
 	{
-		x = new LS(x)
+		x = new LSnum(x)
 		var expDiff = this.l - x.l
-		if (expDiff >= 15 || expDiff <= -15) return { l: Math.max(this.l, x.l) }
-		return { l: x.l + Math.log10(1 + Math.pow(10, expDiff)) }
+		if (expDiff >= 15) return this
+		if (expDiff <= -15) return x
+		this.l = this.l + Math.log10(1 + Math.pow(10, expDiff))
+		return x
 	}
 
 	sub(x)
 	{
-		x = new LS(x)
+		x = new LSnum(x)
 		var expDiff = this.l - x.l
-		if (expDiff < 0) return { l: Number.NEGATIVE_INFINITY }
-		if (expDiff >= 15) return { l: this.l }
-		return { l: x.l + Math.log10(1 - Math.pow(10, -expDiff)) }
+		if (expDiff < 0)
+		{
+			this.l = -Infinity
+			return this
+		}
+		if (expDiff >= 15 || x.l == -Infinity) return this
+		this.l = this.l + Math.log10(1 - Math.pow(10, -expDiff))
+		return this
 	}
 
 	mul(x)
 	{
-		x = new LS(x)
-		return { l: this.l + x.l}
+		x = new LSnum(x)
+		this.l = this.l + x.l
+		return this
 	}
 
 	div(x)
 	{
-		x = new LS(x)
-		return { l: this.l - x.l}
+		x = new LSnum(x)
+		this.l = this.l - x.l
+		return this
 	}
 
 	pow(x)
 	{
-		return { l: this.l * x }
-	}
-
-	sqr()
-	{
-		return { l: this.l * 2 }
-	}
-
-	cub()
-	{
-		return { l: this.l * 3 }
+		this.l = this.l * x
+		return this
 	}
 
 	root(x)
 	{
-		return { l: this.l / x }
-	}
-
-	sqrt()
-	{
-		return { l: this.l / 2 }
-	}
-
-	cbrt()
-	{
-		return { l: this.l / 3 }
-	}
-
-	log10()
-	{
-		return { l: Math.log10(this.l)}
-	}
-
-	log2()
-	{
-		return { l: this.l / 3.32192809488736234787 }
+		this.l = this.l / x
+		return this
 	}
 
 	log(x)
 	{
-		x = new LS(x)
-		return { l: this.l / x.l }
+		this.l = this.l / Math.log10(x)
+		return new LSnum(this.l)
 	}
 
 	floor()
 	{
-		if (this.l < 0) return { l: Number.NEGATIVE_INFINITY }
-		if (this.l < 15) return { l: Math.log10(Math.floor(Math.pow(10, this.l) + Math.pow(10, this.l - 14))) }
+		if (this.l < 0) this.l = -Infinity
+		else if (this.l < 15) this.l = Math.log10(Math.floor(Math.pow(10, this.l) + Math.pow(10, this.l - 14)))
+		return this
 	}
 
 	ceil()
 	{
-		if (this.l == Number.NEGATIVE_INFINITY) return { l: Number.NEGATIVE_INFINITY }
-		if (this.l < 0) return { l: 0 }
-		if (this.l < 15) return { l: Math.log10(Math.ceil(Math.pow(10, this.l) - Math.pow(10, this.l - 14))) }
+		if (this.l == -Infinity) this.l = -Infinity
+		else if (this.l < 0) this.l = 0
+		else if (this.l < 15) this.l = Math.log10(Math.ceil(Math.pow(10, this.l) - Math.pow(10, this.l - 14)))
+		return this
 	}
 
 	round()
 	{
-		if (this.l <= -1) return { l: Number.NEGATIVE_INFINITY }
-		if (this.l < 15) return { l: Math.log10(Math.round(Math.pow(10, this.l))) }
+		if (this.l <= -0.30102999566398) this.l = -Infinity
+		if (this.l < 15) return this.l = Math.log10(Math.round(Math.pow(10, this.l)))
+		return this
 	}
 
 	toFixed(x)
 	{
 		if (this.l < -x - 1) return (0).toFixed(x)
-		if (this.l == Number.POSITIVE_INFINITY) return 'Infinity'
+		if (this.l == Infinity) return 'Infinity'
 		if (this.l >= 1e21) return 'e' + this.l
 		if (this.l >= 21) return Math.pow(10, this.l - Math.floor(this.l)).toFixed(x) + 'e' + Math.floor(this.l)
 		return Math.pow(10, this.l).toFixed(x)
 	}
 
+	toNumber()
+	{
+		return Math.pow(10, this.l)
+	}
+
 	min(x)
 	{
-		x = new LS(x)
-		return { l: Math.min(this.l, x.l) }
+		x = new LSnum(x)
+		this.l = Math.min(this.l, x.l)
+		return this
 	}
 
 	max(x)
 	{
-		x = new LS(x)
-		return { l: Math.max(this.l, x.l) }
-	}
-
-	cmp(x)
-	{
-		x = new LS(x)
-		if (this.l > x.l) return 1
-		if (this.l == x.l) return 0
-		if (this.l < x.l) return -1
+		x = new LSnum(x)
+		this.l = Math.max(this.l, x.l)
+		return this
 	}
 
 	lt(x)
 	{
-		x = new LS(x)
+		x = new LSnum(x)
 		return this.l < x.l
 	}
 
 	lte(x)
 	{
-		x = new LS(x)
+		x = new LSnum(x)
 		return this.l <= x.l
 	}
 
 	eq(x)
 	{
-		x = new LS(x)
+		x = new LSnum(x)
 		return this.l == x.l
 	}
 
 	gt(x)
 	{
-		x = new LS(x)
+		x = new LSnum(x)
 		return this.l > x.l
 	}
 
 	gte(x)
 	{
-		x = new LS(x)
+		x = new LSnum(x)
 		return this.l >= x.l
 	}
 
 	get e()
 	{
-		if (this.l == Number.NEGATIVE_INFINITY) return 0
+		if (this.l == -Infinity) return 0
 		return Math.floor(this.l)
 	}
 
 	get m()
 	{
-		if (this.l == Number.NEGATIVE_INFINITY) return 0
+		if (this.l == -Infinity) return 0
 		return Math.pow(10, this.l - Math.floor(this.l))
 	}
 }
