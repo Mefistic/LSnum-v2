6e65e4468b3e53d11f52a36c209cd8a49089897f
Mefistic <92448706+Mefistic@users.noreply.github.com>
2022-12-18 07:10:34 -0300

    ROUDNING GERROOORS AAAAAAAAAAAAA

--- LSnum.js
+++ LSnum.js
@@ -91,28 +91,28 @@ class LSnum
 	floor()
 	{
 		var ret = new LS(this)
-		if (ret.l < 15) ret.l = Math.log10(Math.floor(Math.pow(10, ret.l)))
+		if (ret.l < 15) ret.l = Math.log10(Math.floor(Math.pow(10, ret.l + LS_EPSILON)))
 		return ret
 	}
 
 	ceil()
 	{
 		var ret = new LS(this)
-		if (ret.l < 15) ret.l = Math.log10(Math.ceil(Math.pow(10, ret.l)))
+		if (ret.l < 15) ret.l = Math.log10(Math.ceil(Math.pow(10, ret.l + LS_EPSILON)))
 		return ret
 	}
 
 	trunc()
 	{
 		var ret = new LS(this)
-		if (ret.l < 15) ret.l = Math.log10(Math.trunc(Math.pow(10, ret.l)))
+		if (ret.l < 15) ret.l = Math.log10(Math.trunc(Math.pow(10, ret.l + LS_EPSILON)))
 		return ret
 	}
 
 	round()
 	{
 		var ret = new LS(this)
-		if (ret.l < 15) ret.l = Math.log10(Math.round(Math.pow(10, ret.l)))
+		if (ret.l < 15) ret.l = Math.log10(Math.round(Math.pow(10, ret.l + LS_EPSILON)))
 		return ret
 	}
 
